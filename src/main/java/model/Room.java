ae8fc5dec1c5fc02f2e57e6f73727b6f5bd4d05e
alexb <alexbondar03@gmail.com>
2022-12-12 15:41:55 +0300

    Add Dao and model for room

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -1,4 +1,18 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class Room {
+    private int id;
+    private int number;
+    private boolean isReserved;
+
+    public Room(int number, boolean isReserved) {
+        this.number = number;
+        this.isReserved = isReserved;
+    }
 }
