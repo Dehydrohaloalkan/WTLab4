f8475fed56407c03cc1e115c9e8eb61f3640786a
alexb <alexbondar03@gmail.com>
2022-12-12 15:33:15 +0300

    Add Dao and model for user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,22 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private int id;
+    private String login;
+    private String password;
+    private int role;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
